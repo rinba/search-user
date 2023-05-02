b1816cf8e2a0eafac82c2fd68ff12588a4a18647
rinba <q3255620496@outlook.com>
2023-05-02 16:21:19 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  /*配置单个代理服务器
+  devServer:{
+    proxy:'http://localhost:5000'
+  }  */
+
+  /*配置多个代理服务器 
+  devServer:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:5000',
+        pathRewrite:{'^/api':''},
+        //ws:true, //用于支持websocket双向通信
+        //changeOrigin:ture, //用于控制请求头中的host值为一个随机值
+      },
+      '/demo':{
+        target:'http://localhost:5001',
+        pathRewrite:{'^/demo':''},
+      }
+    }
+  }  */
+
+
 })
